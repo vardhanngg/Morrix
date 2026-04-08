279e95757479b8817da1a70e663a4257b1038a79
G Vardhan <gvardhan02@gmail.com>
2026-04-08 15:47:06 +0530

    mail

--- src/main/java/com/morix/util/MailUtil.java
+++ src/main/java/com/morix/util/MailUtil.java
@@ -51,7 +51,7 @@ public class MailUtil {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth",            "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable",      "true");   // SSL on connect (port 465)
         props.put("mail.smtp.host",            HOST);
         props.put("mail.smtp.port",            PORT);
         props.put("mail.smtp.ssl.trust",       HOST);
